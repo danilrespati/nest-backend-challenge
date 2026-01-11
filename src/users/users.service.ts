bb979a192fa6ea58ffbb1f1bfa2c20309aae2d38
Taufiq Dhanial Respati <taufiq.dhanil@gmail.com>
2026-01-11 16:34:28 +0700

    add follow service
    The interview session ends here

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { In, Repository } from 'typeorm';
 import { User, UserRole } from './user.entity';
 import * as bcrypt from 'bcrypt';
 
@@ -9,7 +9,7 @@ export class UsersService {
     constructor(
         @InjectRepository(User)
         private usersRepository: Repository<User>,
-    ) { }
+    ) {}
 
     async findOne(username: string): Promise<User | null> {
         return this.usersRepository.findOne({ where: { username } });
@@ -27,5 +27,39 @@ export class UsersService {
         user.password = await bcrypt.hash(user.password, salt);
         // Default role is user, can be overridden if provided
         return this.usersRepository.save(user);
+    }
+
+    async follow(
+        follower: string,
+        followed: string,
+    ): Promise<{ message: string }> {
+        try {
+            const followerUser = await this.usersRepository.findOne({where: {username: follower}})
+            const followedUser = await this.usersRepository.findOne({where: {username: followed}})
+
+            await this.usersRepository
+                .createQueryBuilder()
+                .update(User)
+                .set({
+                    follower: () => 'array_append(follower, :followerId)',
+                })
+                .setParameter("followerId", followerUser?.id)
+                .where('id = :id', { id: followedUser?.id })
+                .execute();
+
+            await this.usersRepository
+                .createQueryBuilder()
+                .update(User)
+                .set({
+                    following: () => 'array_append(following, :followedId)',
+                })
+                .setParameter("followedId", followedUser?.id)
+                .where('id = :id', { id: followerUser?.id })
+                .execute();
+            return { message: 'Success' };
+        } catch (error) {
+            console.error(error);
+            throw new InternalServerErrorException('Something went wrong');
+        }
     }
 }
